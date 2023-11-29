bf1b9fd7880b9c61090e0424a010d599ee25c3bb
MohamedH1000 <mohammedhisham115@gmail.com>
2023-11-29 18:32:35 +0000

    iterator through objects

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-   let itemstr = '', i = 0;
-   for (const item of reportWithIterator) {
-        if (reportWithIterator.length - 1 == i) {
-            itemstr += item
-        }
-        else {
-            itemstr += item + ' | ';
-        }
-        i++;
-   }
-   return itemstr;
+  return [...reportWithIterator].join(' | ');
 }
