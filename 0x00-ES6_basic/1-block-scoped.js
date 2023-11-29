1b28ab90c9612549833f548e888f6741dc9746a6
MohamedH1000 <mohammedhisham115@gmail.com>
2023-11-29 18:28:35 +0000

    block-scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,11 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    /* no unused variables by eslint-disabled */
-    const task = true;
-    const task2 = false;
-    /* eslint-enable no-unused-vars */
+    return [task, task2];
   }
-
   return [task, task2];
 }
